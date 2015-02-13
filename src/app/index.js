f5cd03bca233d0eab8db186949b47fcba234559b
swong <swong@broadbandtvcorp.com>
2015-02-12 16:33:03 -0800

    bootstrapped polymer element loading for ops sister ;)

--- src/app/index.js
+++ src/app/index.js
@@ -4,18 +4,31 @@
 import MainCtrl from './main/main.controller';
 import NavbarCtrl from '../components/navbar/navbar.controller';
 
-angular.module('tribes', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'restangular', 'ui.router'])
-  .controller('MainCtrl', MainCtrl)
-  .controller('NavbarCtrl', NavbarCtrl)
-  
-  .config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
-      });
+var deps = ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'restangular', 'ui.router', 'ng-polymer-elements']
 
-    $urlRouterProvider.otherwise('/');
-  })
-;
+var modules = [
+  'Profile',
+  'Library'
+];
+
+var loadModules = function() {
+  deps = deps.concat(modules);
+
+  var tribes = angular.module('tribes', deps.)
+    .controller('MainCtrl', MainCtrl)
+    .controller('NavbarCtrl', NavbarCtrl)
+    
+    .config(function ($stateProvider, $urlRouterProvider) {
+      $stateProvider
+        .state('home', {
+          url: '/',
+          templateUrl: 'app/main/main.html',
+          controller: 'MainCtrl'
+        });
+
+      $urlRouterProvider.otherwise('/');
+    })
+  ;
+};
+
+loadModules();
